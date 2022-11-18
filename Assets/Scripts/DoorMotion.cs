c2362efc3d0f1ff62326d4e108775fdda4ae2025
Shy Ohev Zion <48812370+ShyOZ@users.noreply.github.com>
2022-11-18 14:20:55 +0200

    door animation - wonky but sorta works?

--- Assets/Scripts/DoorMotion.cs
+++ Assets/Scripts/DoorMotion.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class DoorMotion : MonoBehaviour
 {
     Animator anim;
+    int dir = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
-
+        anim = this.gameObject.GetComponentInParent<Animator>();
+        dir = (this.gameObject.name.StartsWith("Front")) ? -1 : 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider other)
     {
+        anim.SetInteger("dir", dir);
         anim.SetBool("OpenDoor", true);
+        Debug.Log("Door is opening - " + anim.GetInteger("dir") + " " + anim.GetBool("OpenDoor"));
+
     }
 
     void OnTriggerExit(Collider other)
     {
+        anim.SetInteger("dir", 0);
         anim.SetBool("OpenDoor", false);
+        Debug.Log("Door is closing - " + anim.GetInteger("dir") + " " + anim.GetBool("OpenDoor"));
     }
 }
