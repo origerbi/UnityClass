b960d860a5acd2015c77142e6293aaba30bdafdc
Shy Ohev Zion <48812370+ShyOZ@users.noreply.github.com>
2022-12-02 17:04:34 +0200

    key not animated fixed (hopefully) and other shit

--- Assets/Scripts/SpawnKey.cs
+++ Assets/Scripts/SpawnKey.cs
@@ -6,14 +6,16 @@ public class SpawnKey : MonoBehaviour
 {
     Animator anim;
     public GameObject key;
+    public RuntimeAnimatorController keyAnimatorController;
     private bool keySpawned = false;
     private bool keyPickedUp = false;
-    private Vector3 keyRotation = new Vector3(0, -90f, 90f);
+    private Vector3 keyRotation;
     private float keyScale = 0.7f;
 
     void Start()
     {
         anim = this.gameObject.GetComponentInParent<Animator>();
+        keyRotation = this.gameObject.transform.rotation.eulerAngles - new Vector3(0, 90f, 90f);
     }
 
     void Update()
@@ -21,10 +23,10 @@ public class SpawnKey : MonoBehaviour
         if (anim.GetBool("openChest") && !keySpawned)
         {
             keySpawned = true;
-            Instantiate(key, this.gameObject.transform.position, Quaternion.Euler(keyRotation));
-            key.transform.localScale = new Vector3(keyScale, keyScale, keyScale);
-            Animator keyAnimator = key.AddComponent<Animator>();
-            keyAnimator.runtimeAnimatorController = Resources.Load("KeyAnimator") as RuntimeAnimatorController;
+            GameObject k = Instantiate(key, this.gameObject.transform.position, Quaternion.Euler(keyRotation));
+            k.transform.localScale = new Vector3(keyScale, keyScale, keyScale);
+            Animator keyAnimator = k.AddComponent<Animator>();
+            keyAnimator.runtimeAnimatorController = keyAnimatorController;
         }
     }
 }
